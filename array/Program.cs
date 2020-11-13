06d6aa72f05c4acfb94f218786805a16ca82c579
Left_Tapochek <serregasavinykh15@gmail.com>
2020-11-13 22:44:45 +0500

    Работа с массивами

--- array/Program.cs
+++ array/Program.cs
@@ -28,57 +28,74 @@ namespace array
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("\nОдномерный массив в столбец:");
             Console.ResetColor();
-            for (int i = 0; i < num.Length; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
                 Console.Write("\nВведите элемент массива под индексом {0}:\t", i + 1);
-                num[i] = int.Parse(Console.ReadLine());
+                numbers[i] = int.Parse(Console.ReadLine());
             }
             Console.WriteLine("\nВывод массива:");
-            for (int i = 0; i < num.Length; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine(num[i]);
+                Console.WriteLine(numbers[i]);
             }
             Console.WriteLine("------------------------------------------");
-
-            Console.ReadKey();
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Одномерный массив в диагональ:");
+            Console.WriteLine("\nОдномерный массив в диагональ:");
             Console.ResetColor();
-            for (int i = 0; i < num.Length; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
                 Console.Write("\nВведите элемент массива под индексом {0}:\t", i + 1);
-                num[i] = int.Parse(Console.ReadLine());
+                numbers[i] = int.Parse(Console.ReadLine());
             }
             Console.WriteLine("\nВывод массива:");
             int n = 0;
-            for (int i = 0; i < num.Length; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
                 string v = "".PadLeft(n, ' ');
-                Console.WriteLine(v + num[i]);
+                Console.WriteLine(v + numbers[i]);
                 n++;
             }
             Console.WriteLine("------------------------------------------");
-            Console.WriteLine("\nХотите попробывать еще раз? 1 - да, 0 - нет");
-            int repeat = int.Parse(Console.ReadLine());
-            switch (repeat)
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nОдномерный массив в диагональ (обратная сторона):");
+            Console.ResetColor();
+            for (int i = 0; i < numbers.Length; i++)
             {
-                case 1:
-                    Console.Clear();
-                    Main();
-                    break;
-                case 0:
-                    Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("\nСпасибо, что использовали мою программу, можете поддержать этот проект денежными средствами :)");
-                    Console.ResetColor();
-                    break;
+                Console.Write("\nВведите элемент массива под индексом {0}:\t", i + 1);
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\nВывод массива:");
+            //int n = 0;
+            int t = numbers.Length;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                string v = "".PadLeft(n, ' ');
+                Console.WriteLine(v + numbers[i]);
+                //n++;
+                n--;
+            }
+            Console.WriteLine("------------------------------------------");
+                Console.WriteLine("\nХотите попробывать еще раз? 1 - да, 0 - нет");
+                int repeat = int.Parse(Console.ReadLine());
+                switch (repeat)
+                {
+                    case 1:
+                        Console.Clear();
+                        Main();
+                        break;
+                    case 0:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nСпасибо, что использовали мою программу, можете поддержать этот проект денежными средствами :)");
+                        Console.ResetColor();
+                        break;
+
+                }
+
+                Console.ReadKey();
+
+
 
             }
-
-            Console.ReadKey();
-
-
-
         }
     }
-}
