f1497354a957dffde4fc30f7c905f79edea4c511
Left_Tapochek <serregasavinykh15@gmail.com>
2020-11-13 20:35:43 +0500

    Третья демо-версия

--- array/Program.cs
+++ array/Program.cs
@@ -9,25 +9,38 @@ namespace array
             Console.Write("Введите кол-во элементов массива:\t");
             int elementCount = int.Parse(Console.ReadLine());
             int[] nombers = new int[elementCount];
-            Console.WriteLine("Одномерный массив в строку");
+            int[] numbers = new int[elementCount];
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("\nОднмерный массив в строку:");
             for (int i = 0; i < nombers.Length; i++)
             {
                Console.Write("\nВведите элемент массива под индексом {0}:\t", i+1);
                 nombers[i] = int.Parse(Console.ReadLine());
-                Console.WriteLine(i);
             }
-            
-            int[] numbers = new int[10];
-            Console.WriteLine("Одномерный массив в столбец");
+            Console.WriteLine("\nВывод массива:");
+            for (int i = 0; i < nombers.Length; i++)
+            {
+                Console.Write(nombers[i]);
+            }
+            Console.WriteLine("\n------------------------------------------");
+            Console.WriteLine("\nОдномерный массив в столбец:");
             for (int i = 0; i < numbers.Length; i++)
             {
-                Console.Write("\nВведите элемент массива под индексом {0}:\t", i+1);
+                Console.Write("\nВведите элемент массива под индексом {0}:\t", i + 1);
                 numbers[i] = int.Parse(Console.ReadLine());
-                Console.WriteLine(i);
+                            }
+            Console.WriteLine("\nВывод массива:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
             }
+            Console.WriteLine("------------------------------------------");
+
             Console.ReadKey();
             Console.WriteLine("Одномерный массив в диагональ:");
-            Console.WriteLine("Не нашел как :/");
+            Console.WriteLine("Не нашел как сделать :/");
+            Console.WriteLine("------------------------------------------");
+            Console.ReadKey();
 
 
 
