7c93a807f626a06b7e273781d5c023b942c1153b
Left_Tapochek <serregasavinykh15@gmail.com>
2020-11-13 20:06:26 +0500

    Первая демо-версия

--- array/Program.cs
+++ array/Program.cs
@@ -6,7 +6,25 @@ namespace array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nombers = new int[10];
+            Console.WriteLine("Одномерный массив в строку:");
+            for (int i = 0; i < nombers.Length; i++)
+            {
+               Console.Write(i);
+            }
+            Console.ReadKey();
+            int[] numbers = new int[10];
+            Console.WriteLine("Одномерный массив в столбец");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+            Console.WriteLine("Одномерный массив в диагональ:");
+            Console.WriteLine("Не нашел как :/");
+
+
+
         }
     }
 }
