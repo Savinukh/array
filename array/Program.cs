c1f42a49ef3f724adfe3f6cf2a717143f0e209d6
Left_Tapochek <serregasavinykh15@gmail.com>
2020-11-13 20:17:16 +0500

    Вторая демо-версия

--- array/Program.cs
+++ array/Program.cs
@@ -6,17 +6,23 @@ namespace array
     {
         static void Main(string[] args)
         {
-            int[] nombers = new int[10];
-            Console.WriteLine("Одномерный массив в строку:");
+            Console.Write("Введите кол-во элементов массива:\t");
+            int elementCount = int.Parse(Console.ReadLine());
+            int[] nombers = new int[elementCount];
+            Console.WriteLine("Одномерный массив в строку");
             for (int i = 0; i < nombers.Length; i++)
             {
-               Console.Write(i);
+               Console.Write("\nВведите элемент массива под индексом {0}:\t", i+1);
+                nombers[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine(i);
             }
-            Console.ReadKey();
+            
             int[] numbers = new int[10];
             Console.WriteLine("Одномерный массив в столбец");
             for (int i = 0; i < numbers.Length; i++)
             {
+                Console.Write("\nВведите элемент массива под индексом {0}:\t", i+1);
+                numbers[i] = int.Parse(Console.ReadLine());
                 Console.WriteLine(i);
             }
             Console.ReadKey();
